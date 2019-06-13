bf73ec79e48b07e37ab6a330244cd688805a343f
Firhat Hidayat <firhathidayat@tutanota.com>
2019-06-13 20:09:25 +0700

    - Fix map position - fix map zoom

---
+++ JSmart/JSmart/View Controllers/SplashScreenViewControler.swift
@@ -0,0 +1,14 @@
+//
+//  SplashScreenViewControler.swift
+//  JSmart
+//
+//  Created by lembah7 on 13/06/19.
+//  Copyright © 2019 Firhat. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SplashScreenViewControler: UIViewController{
+    
+}
